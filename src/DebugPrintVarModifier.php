965411782f18ff977e1ed285d8b69e2eeffd593b
Raimondas Rimkevičius (aka MekDrop) <github@mekdrop.name>
2025-05-15 00:40:27 +0300

    Bump min PHP version to 8.3

--- src/DebugPrintVarModifier.php
+++ src/DebugPrintVarModifier.php
@@ -6,27 +6,13 @@ use ErrorException;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\CliDumper;
 use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+use Symfony\Component\VarDumper\Dumper\AbstractDumper;
 
-/**
- * This describes Smarty debug_print_var var modifier based on symfony var_dumper
- *
- * @package Imponeer\Smarty\Extensions\Dump
- */
 class DebugPrintVarModifier
 {
-    /**
-     * @var VarCloner
-     */
-    private $cloner;
+    private readonly VarCloner $cloner;
+    private readonly AbstractDumper $dumper;
 
-    /**
-     * @var HtmlDumper
-     */
-    private $dumper;
-
-    /**
-     * DebugPrintVarModifier constructor.
-     */
     public function __construct()
     {
         $this->cloner = new VarCloner();
@@ -36,13 +22,10 @@ class DebugPrintVarModifier
     /**
      * Executes plugin function
      *
-     * @param mixed $var Variable to print
-     *
-     * @return string
-     *
      * @throws ErrorException
      */
-    public function __invoke($var): string {
+    public function __invoke(mixed $var): string
+    {
         $memoryStream = fopen('php://memory', 'r+b');
 
         $this->dumper->dump(
